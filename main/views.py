164a138f2e0991a8e9464135d51c257ce56fe1ed
kinsanghun <52707244+kinsanghun@users.noreply.github.com>
2022-04-21 01:15:00 +0900

    init

--- main/views.py
+++ main/views.py
@@ -14,13 +14,40 @@ def reports(request):
 
 def companylist(request):
     page = request.GET.get('page', '1')  # 페이지
-    datas = company.objects.order_by("-year")
+    select_company = request.GET.get('entrpsNm', 'none')
+    print(select_company)
 
+    chckAt = request.GET.get("chckAt", 'Y')
+
+    if request.method == "POST":
+        if request.POST["entrpsNm"] == "none":
+            datas = company.objects.filter(chckAt=request.POST["chckAt"]).values().order_by("-year")
+            chckAt = request.POST["chckAt"]
+
+
+        else:
+            datas = company.objects.filter(entrpsNm=request.POST["entrpsNm"],
+                                        chckAt=request.POST["chckAt"]).values().order_by("-year")
+            select_company = request.POST["entrpsNm"]
+            chckAt = request.POST["chckAt"]
+
+    else:
+        if select_company == "none":
+            datas = company.objects.filter(chckAt=chckAt).values().order_by("-year")
+        else:
+            datas = company.objects.filter(entrpsNm=select_company, chckAt=chckAt).values().order_by("-year")
+
+        chckAt = "Y"
+
+    cname = company.objects.values("entrpsNm").distinct()
     paginator = Paginator(datas, 10)
     page_obj = paginator.get_page(page)
 
     context = {
-        'datas' : page_obj
+        'datas' : page_obj,
+        'cname' : cname,
+        'entrpsNm' : select_company,
+        'chckAt' : chckAt
     }
     return render(request, "company/company.html", context)
 
